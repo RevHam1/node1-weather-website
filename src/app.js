e69ae93174bc5e5595062c2288108294b8deff8c
RevHam1 <oldhamtyrone@yahoo.com>
2019-09-24 04:23:16 -0500

    Added images and changed image to oval and centered text

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ app.get("*", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
