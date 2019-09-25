e5808255269c2879e2dd0f253518ed011d03c79a
RevHam1 <oldhamtyrone@yahoo.com>
2019-09-25 02:30:53 -0500

    Deleted unnecessary img files updated app with more style

--- src/app.js
+++ src/app.js
@@ -28,14 +28,14 @@ app.get("", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "About Me",
+    title: "About This App",
     name: "Tyrone Oldham"
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    message: "This is the help page :)",
+    message: "This is the help page.",
     title: "Help",
     name: "Tyrone Oldham"
   });
